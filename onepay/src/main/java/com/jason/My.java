1493da6eb0138ffc81ecf560a426ef460aab6497
jasonjyfandjtt <jyfandjtt@sina.com>
2022-09-12 11:08:56 +0800

    Initial commit

---
+++ onepay/src/main/java/com/jason/My.java
@@ -0,0 +1,9 @@
+package com.jason;
+
+public class My {
+    public static void main(String[] args) {
+    String a="Adg";
+        String s = a.toLowerCase();
+        System.out.println(s);
+    }
+}
